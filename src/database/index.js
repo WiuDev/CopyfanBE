b51d1569ea09a797edbf091109700870e0df4414
Willian Eduardo <willianeduardo9877@hotmail.com>
2025-12-02 17:16:35 -0300

    feat: alterações conexão db

--- src/database/index.js
+++ src/database/index.js
@@ -1,17 +1,37 @@
-const {Sequelize} = require("sequelize");
+const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const HOST = process.env.DB_HOST;
-const USER = process.env.DB_USER;
-const PASSWORD = process.env.DB_PASSWORD;
-const DB = process.env.DB_NAME;
-const PORT = process.env.DB_PORT
+const DATABASE_URL = process.env.DATABASE_URL;
 
-const sequelize = new Sequelize(DB, USER, PASSWORD, {
-    host: HOST,
-    port: PORT,
-    dialect: "postgres",
-    logging: false,
-});
+if (DATABASE_URL) {
 
-module.exports = sequelize;
+    const sequelize = new Sequelize(DATABASE_URL, {
+        dialect: 'postgres',
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true, 
+                rejectUnauthorized: false
+            }
+        },
+    });
+
+    module.exports = sequelize;
+
+} else {
+    // Forma Local/Antiga
+    const HOST = process.env.DB_HOST;
+    const USER = process.env.DB_USER;
+    const PASSWORD = process.env.DB_PASSWORD;
+    const DB = process.env.DB_NAME;
+    const PORT = process.env.DB_PORT;
+
+    const sequelize = new Sequelize(DB, USER, PASSWORD, {
+        host: HOST,
+        port: PORT,
+        dialect: "postgres",
+        logging: false,
+    });
+
+    module.exports = sequelize;
+}
