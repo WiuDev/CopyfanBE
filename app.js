d0c7f4fc557188bddd6cd719889515c3ae2856c4
Willian Eduardo <willianeduardo9877@hotmail.com>
2025-12-03 13:26:45 -0300

    feat: Teste de timeout para celulares lentos

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setTimeout(15000, () => {
+    return res.status(408).json({ error: "Request timeout" });
+  });
+  next();
+});
 app.use('/', routes);
 
 module.exports = app;
